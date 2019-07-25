323cb202a4c1e603f2e401e833df3f8e7e525371
Chris Snyder <chris.snyder@triadhq.com>
2019-07-25 14:55:08 -0700

    First commit

---
+++ resources/views/weather.blade.php
@@ -0,0 +1,36 @@
+{{--<h1>Weather Forecast <span class="text-muted">|</span> {{($geoIParr['city'].', '.$geoIParr['state'])}}</h1>--}}
+
+
+{{--{{dd($geoIParr)}}--}}
+
+
+{{--{{dd($forecastData)}}--}}
+
+    @foreach($forecastData['list'] as $forecast)
+
+        <h2>{{date('l F dS Y gA',strtotime($forecast['dt_txt'])) }}</h2>
+
+
+    @foreach($forecast['weather'] as  $weather )
+
+       {{--{{dd($weather)}}--}}
+
+      <p><img src="http://openweathermap.org/img/wn/{{$weather['icon']}}@2x.png" title="{{$weather['main']}}" alt="{{$weather['main']}}"></p>
+
+      <p>{{ucwords($weather['description'])}}</p>
+
+
+    @endforeach
+
+    <p>Temp: {{$forecast['main']['temp']}}&#8457;</p>
+    <p>Min Temp: {{$forecast['main']['temp_min']}}&#8457;  Max Temp:  {{$forecast['main']['temp_max']}}&#8457;</p>
+    <p>Pressure:  {{$forecast['main']['pressure']}}</p>
+    <p>Sea Level:  {{$forecast['main']['sea_level']}}</p>
+    <p>Ground Level:  {{$forecast['main']['grnd_level']}}</p>
+    <p>Humidity:  {{$forecast['main']['humidity']}}</p>
+    <p>Temp Kf:  {{$forecast['main']['temp_kf']}}</p>
+
+        <br/>
+        <hr/>
+
+    @endforeach
---
+++ routes/web.php
@@ -0,0 +1,68 @@
+<?php
+
+use GuzzleHttp\Client;
+
+use Illuminate\Support\Facades\Cache;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//
+//    //gather user's ip address...
+//    $userIP = request()->ip();
+//
+//    //get location based on ip address (geoIP package)...
+//    $geoIParr = geoip()->getLocation($userIP);
+//
+//
+//    $userCity = $geoIParr['city'];
+//    $userState = $geoIParr['state'];
+//
+//    $userLatitude = $geoIParr['lat'];
+//    $userLongitude = $geoIParr['lon'];
+//
+//
+//    //add to ENV file...
+////    $ACCESS_TOKEN = env('STATS_API_KEY');
+//      $ACCESS_TOKEN = 'ad463d4c462f97b7da391ddf58a33d69';
+//
+//
+//      //instantiate Guzzle client...
+//
+//    $client = new Client();
+//
+//
+//    //pass in city & state based on IP?
+//
+//    $url = 'api.openweathermap.org/data/2.5/forecast?lat='.$userLatitude.'&lon='.$userLongitude.'&units=imperial&APPID='.$ACCESS_TOKEN.'';
+//
+//    $response = $client->request('GET', $url);
+//
+//    //$forecastData = $response->getBody()->getContents();
+//
+//    $forecastData = json_decode($response->getBody(), true);
+//
+//
+//  //  $forecastData = json_decode($forecastData, true);
+//
+//    //$forecastData = json_encode($forecastData);
+//
+//
+//
+//
+//
+//    return view('weather', compact('geoIParr', 'userIP', 'forecastData'));
+//});
+
+
+
+Route::get('/weather', ['as' => 'weather', 'uses' => 'WeatherController@forecast']);
