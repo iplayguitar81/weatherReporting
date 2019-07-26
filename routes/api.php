1cee1a131c576428d84a4cc0bfecc3c5989c0815
Chris Snyder <chris.snyder@triadhq.com>
2019-07-25 22:51:56 -0700

    Adjustments to the web routing and initial storing of API routing.

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//API route to request forecasts based on user's ip address/lat and longitude...
+Route::get('forecasts', 'WeatherController@vueForecast');
--- routes/web.php
+++ routes/web.php
@@ -15,55 +15,12 @@ use Illuminate\Support\Facades\Cache;
 |
 */
 
-//Route::get('/', function () {
-//
-//    //gather user's ip address...
-//    $userIP = request()->ip();
-//
-//    //get location based on ip address (geoIP package)...
-//    $geoIParr = geoip()->getLocation($userIP);
-//
-//
-//    $userCity = $geoIParr['city'];
-//    $userState = $geoIParr['state'];
-//
-//    $userLatitude = $geoIParr['lat'];
-//    $userLongitude = $geoIParr['lon'];
-//
-//
-//    //add to ENV file...
-//    $ACCESS_TOKEN = env('OPEN_WEATHER_API_KEY');
-//
-//
-//
-//      //instantiate Guzzle client...
-//
-//    $client = new Client();
-//
-//
-//    //pass in city & state based on IP?
-//
-//    $url = 'api.openweathermap.org/data/2.5/forecast?lat='.$userLatitude.'&lon='.$userLongitude.'&units=imperial&APPID='.$ACCESS_TOKEN.'';
-//
-//    $response = $client->request('GET', $url);
-//
-//    //$forecastData = $response->getBody()->getContents();
-//
-//    $forecastData = json_decode($response->getBody(), true);
-//
-//
-//  //  $forecastData = json_decode($forecastData, true);
-//
-//    //$forecastData = json_encode($forecastData);
-//
-//
-//
-//
-//
-//    return view('weather', compact('geoIParr', 'userIP', 'forecastData'));
-//});
 
+//vue route...
+Route::get('/', function () {
 
+    return view('weather-vue');});
 
-
+//laravel route...
 Route::get('/weather', ['as' => 'weather', 'uses' => 'WeatherController@forecast']);
+
